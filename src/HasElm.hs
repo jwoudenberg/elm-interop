5836309cb7ef3934c7834b1f0f1fcc40c1fc31b9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2018-10-20 22:46:41 +0100

    Ensure `HasElmRec` is also a `HasElm`
    Because an elm record is a valid Elm type. This will make things easier
    when we start defining parameters, because these can be any elm type.
    Now this already includes records!

--- src/HasElm.hs
+++ src/HasElm.hs
@@ -43,11 +43,14 @@ class HasElm a where
   to :: a -> ElmType a
   from :: ElmType a -> a
 
-class HasElmRec a where
-  type Fields a :: [(Symbol, *)]
-  elmTypeRec :: proxy a -> IsElmType (Record Identity (Fields a))
-  toRec :: a -> Record Identity (Fields a)
-  fromRec :: Record Identity (Fields a) -> a
+class (HasElm a, ElmType a ~ Record Identity (Fields' a)) =>
+      HasElmRec' a
+  where
+  type Fields' a :: [(Symbol, *)]
+  type Fields' a = ElmFields (ElmType a)
+
+type family ElmFields a where
+  ElmFields (Record Identity xs) = xs
 
 instance HasElm Int where
   type ElmType Int = Int
@@ -62,39 +65,40 @@ instance (HasElm a) => HasElm (K1 i a p) where
   from = K1 . from
 
 instance forall n su ss ds f p. (KnownSymbol n, HasElm (f p)) =>
-         HasElmRec (M1 S ('MetaSel ('Just n) su ss ds) f p) where
-  type Fields (M1 S ('MetaSel ('Just n) su ss ds) f p) = '[ '( n, ElmType (f p))]
-  elmTypeRec _ = ElmRecord . singleton (Proxy @n) . elmType $ Proxy @(f p)
-  toRec = singleton (Proxy @n) . Identity . to . unM1
-  fromRec = M1 . from . getIdentity . unSingleton
+         HasElm (M1 S ('MetaSel ('Just n) su ss ds) f p) where
+  type ElmType (M1 S ('MetaSel ('Just n) su ss ds) f p) = Record Identity '[ '( n, ElmType (f p))]
+  elmType _ = ElmRecord . singleton (Proxy @n) . elmType $ Proxy @(f p)
+  to = singleton (Proxy @n) . Identity . to . unM1
+  from = M1 . from . getIdentity . unSingleton
 
-instance ( RecAppend (Fields (a p)) (Fields (b p))
-         , HasElmRec (a p)
-         , HasElmRec (b p)
+instance forall n su ss ds f p. (KnownSymbol n, HasElm (f p)) =>
+         HasElmRec' (M1 S ('MetaSel ('Just n) su ss ds) f p)
+
+instance ( RecAppend (Fields' (a p)) (Fields' (b p))
+         , HasElmRec' (a p)
+         , HasElmRec' (b p)
          ) =>
-         HasElmRec ((a :*: b) p) where
-  type Fields ((a :*: b) p) = Fields (a p) +++ Fields (b p)
-  elmTypeRec _ =
-    elmRecAppend (elmTypeRec $ Proxy @(a p)) (elmTypeRec $ Proxy @(b p))
-  toRec (x :*: y) = rappend (toRec x) (toRec y)
-  fromRec rec = fromRec x :*: fromRec y
+         HasElm ((a :*: b) p) where
+  type ElmType ((a :*: b) p) = Record Identity (Fields' (a p) +++ Fields' (b p))
+  elmType _ = elmRecAppend (elmType $ Proxy @(a p)) (elmType $ Proxy @(b p))
+  to (x :*: y) = rappend (to x) (to y)
+  from rec = from x :*: from y
     where
       (x, y) = rsplit rec
 
-elmRecAppend ::
-     (RecAppend xs ys)
-  => IsElmType (Record f xs)
-  -> IsElmType (Record f ys)
-  -> IsElmType (Record f (xs +++ ys))
-elmRecAppend (ElmRecord a) (ElmRecord b) = ElmRecord (rappend a b)
+instance ( RecAppend (Fields' (a p)) (Fields' (b p))
+         , HasElmRec' (a p)
+         , HasElmRec' (b p)
+         ) =>
+         HasElmRec' ((a :*: b) p)
 
 -- | TODO: wrap in constructor
-instance forall f p n fi s. (HasElmRec (f p)) =>
+instance forall f p n fi s. (HasElm (f p)) =>
          HasElm (M1 C ('MetaCons n fi s) f p) where
-  type ElmType (M1 C ('MetaCons n fi s) f p) = Record Identity (Fields (f p))
-  elmType _ = elmTypeRec (Proxy @(f p))
-  to = toRec . unM1
-  from = M1 . fromRec
+  type ElmType (M1 C ('MetaCons n fi s) f p) = ElmType (f p)
+  elmType _ = elmType (Proxy @(f p))
+  to = to . unM1
+  from = M1 . from
 
 -- | TODO: wrap in type name
 instance forall f p c. (HasElm (f p)) => HasElm (M1 D c f p) where
@@ -102,3 +106,10 @@ instance forall f p c. (HasElm (f p)) => HasElm (M1 D c f p) where
   elmType _ = elmType (Proxy @(f p))
   to = to . unM1
   from = M1 . from
+
+elmRecAppend ::
+     (RecAppend xs ys)
+  => IsElmType (Record f xs)
+  -> IsElmType (Record f ys)
+  -> IsElmType (Record f (xs +++ ys))
+elmRecAppend (ElmRecord a) (ElmRecord b) = ElmRecord (rappend a b)
