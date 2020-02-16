5c28e569c12db2ef7438a1870070dada1790f46b
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-02-16 16:15:48 +0000

    Add ormolu

--- shell.nix
+++ shell.nix
@@ -1,6 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell {
+let
+  ormolu = pkgs.haskellPackages.callCabal2nix "ormolu" (pkgs.fetchFromGitHub {
+    owner = "tweag";
+    repo = "ormolu";
+    rev = "3abadaefa5e190ff346f9aeb309465ac890495c2";
+    sha256 = "0vqrb12bsp1dczff3i5pajzhjwz035rxg8vznrgj5p6j7mb2vcnd";
+  }) { };
+in pkgs.mkShell {
   buildInputs = [
     pkgs.haskellPackages.ghcid
     pkgs.haskellPackages.hindent
@@ -8,5 +15,6 @@ pkgs.mkShell {
     pkgs.ncurses
     pkgs.stack
     pkgs.zlib
+    ormolu
   ];
 }
